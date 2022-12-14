a33740dca4d32550f0656c5f181f039561741727
Steven <stevengf14@gmail.com>
2022-12-14 12:00:26 -0500

    Header style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "./HeaderStyle.css";
 
-function Header() {
+function Header(props) {
+  const renderLinks = () => {
+    if (props.authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      );
+    }
+  };
   return (
-    <div>
+    <div className="header">
       <Link to="/">Redux Auth</Link>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signout">Sign Out</Link>
-      <Link to="/feature">Feature</Link>
+      {renderLinks()}
     </div>
   );
 }
-export default Header;
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+export default connect(mapStateToProps)(Header);
